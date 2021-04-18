a52e6113db636866e7e39b2b033dff690fd84dd2
chennakesava reddy <chennakesava.vemireddy@ojas-it.com>
2021-04-18 18:50:42 +0530

    Modified forms and signup template

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -6,3 +6,19 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if email and User.objects.filter(email=email).count():
+            raise forms.ValidationError('This email address is already in use. Please supply a different email address.')
+        return email    
+
+
+    def save(self, commit=True):
+    	user = super(NewUserForm, self).save(commit=False)
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user
+    
